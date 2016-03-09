08f482e466c5239e233c10317f887c03264f8254
Leon Ho <vuonghominh@gmail.com>
2016-03-09 23:00:27 +0700

    Initial Commit

---
+++ tapper-exteme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapper-exteme
+//
+//  Created by LEO on 3/9/16.
+//  Copyright © 2016 vhm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
